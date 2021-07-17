c661f3e94ddbd996db3c8c09552aa288d7fffa2b
Nurgeldi <sertai01@bk.ru>
2021-07-17 21:59:06 +0600

    Initial commit

---
+++ lib/src/screens/auth/auth_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Авторизация'),
+      ),
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CupertinoTextField(
+              placeholder: 'Логин или почта',
+            ),
+            CupertinoTextField(
+              placeholder: 'Пароль',
+            ),
+            CupertinoButton.filled(
+              child: Text('Войти'),
+              onPressed: () {},
+            ),
+            CupertinoButton.filled(
+              child: Text('Зарегистрироваться'),
+              onPressed: () {},
+            ), 
+          ],
+        ),
+      ),
+    );
+  }
+}
