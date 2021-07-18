6baa1a26cdfc552c529baf0a2a402ca49c3a8a4a
Nurgeldi <sertai01@bk.ru>
2021-07-18 16:02:29 +0600

    Initial commit

--- lib/src/screens/auth/auth_screen.dart
+++ lib/src/screens/auth/auth_screen.dart
@@ -6,7 +6,10 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      backgroundColor: Color(0xFFF3F4F6),
       navigationBar: CupertinoNavigationBar(
+      backgroundColor: Color(0xFFF3F4F6),
+      border: Border(),
         middle: Text('Авторизация'),
       ),
       child: SafeArea(
@@ -15,10 +18,20 @@ class AuthScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             CupertinoTextField(
+            decoration: BoxDecoration(
+            color: CupertinoColors.white,
+            ),
               placeholder: 'Логин или почта',
+              padding: const EdgeInsets.symmetric(vertical: 19, horizontal: 16),
+            ),
+             Container(
+              height: 1,
+              color: Color(0xFFE0E6ED),
+              margin: const EdgeInsets.symmetric(horizontal: 16),
             ),
             CupertinoTextField(
               placeholder: 'Пароль',
+              padding: const EdgeInsets.symmetric(vertical: 19, horizontal: 16),
             ),
             SizedBox(height: 32,
             ),
