b54374b1138be1539610c549718963c3d5fa6b27
Nurgeldi <sertai01@bk.ru>
2021-07-19 22:59:20 +0600

    Initial commit

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_appl/src/router/routing_const.dart';
-import 'package:flutter_appl/src/screen/register_screen.dart';
 import 'package:flutter_appl/src/screens/auth/auth_screen.dart';
+import 'package:flutter_appl/src/screens/auth/register_screen.dart';
 
 class AppRouter {
   static Route generateRoute(RouteSettings routeSettings) {
